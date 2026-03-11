848b17702914c5c13f5e8dc80e0b30b93fa282ff
Pouya-Nasraei <122685345+Pouya-Nasraei@users.noreply.github.com>
2026-03-11 15:12:57 +0000

    Add utils script
    Contains data operations and receipt saving

---
+++ utils.py
@@ -0,0 +1,42 @@
+import json
+import logging
+from datetime import datetime
+import os
+
+
+logger = logging.getLogger(__name__)
+
+
+def load_products(path="products.json"):
+
+    try:
+        with open(path, "r") as file:
+            products = json.load(file)
+
+        logger.info("Products loaded successfully")
+        return products
+
+    except FileNotFoundError:
+
+        logger.error("products.json file not found")
+
+        return {}
+
+
+def save_receipt(cart, total_price, final_price):
+
+    os.makedirs("receipts", exist_ok=True)
+
+    filename = f"receipts/receipt_{datetime.now().strftime('%Y%m%d_%H%M%S')}.json"
+
+    receipt_data = {
+        "date": datetime.now().strftime("%Y-%m-%d %H:%M:%S"),
+        "items": cart,
+        "total_before_discount": total_price,
+        "final_price": final_price
+    }
+
+    with open(filename, "w") as file:
+        json.dump(receipt_data, file, indent=4)
+
+    logger.info("Receipt saved: %s", filename)
